d7147b197a5dc8e55989cd2b476aadca4b029718
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2025-01-06 20:28:30 -0600

    main now reads commands and executes them based on the handler function

--- Command.go
+++ Command.go
@@ -36,7 +36,7 @@ func (c *commands) run(s *state, cmd command) error {
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
-		return fmt.Errorf("login expects a single argument, the username")
+		return fmt.Errorf("login expects a username")
 	}
 	err := s.cfg.SetUser(cmd.args[0])
 	if err != nil {
--- main.go
+++ main.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/CrymsonShadows/blog-aggregator/internal/config"
 )
 
 func main() {
 	cfg := config.Read()
-	err := cfg.SetUser("Crymson")
+	s := &state{
+		cfg: &cfg,
+	}
+	cmds := commands{
+		commandsMap: map[string]func(*state, command) error{},
+	}
+	cmds.register("login", handlerLogin)
+	cmdLineArgs := os.Args
+	if len(cmdLineArgs) < 3 {
+		fmt.Println("need more arguments")
+		os.Exit(1)
+	}
+	cmdName := cmdLineArgs[1]
+	cmdArgs := cmdLineArgs[2:]
+	cmd := command{
+		name: cmdName,
+		args: cmdArgs,
+	}
+	err := cmds.run(s, cmd)
 	if err != nil {
 		fmt.Println(err.Error())
+		os.Exit(1)
 	}
-	cfg = config.Read()
-	fmt.Printf("db_url: %s\ncurrent_user_name: %s\n", cfg.DbURL, cfg.CurrentUserName)
 }
