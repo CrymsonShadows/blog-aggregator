31c8bbe473d33f7d5602facca0eb80b71c830f07
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2025-01-26 17:12:33 -0600

    make a more descriptive error message

--- middlewareLoggedIn.go
+++ middlewareLoggedIn.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/CrymsonShadows/blog-aggregator/internal/database"
 )
@@ -10,7 +11,7 @@ func midllewareLoggedIn(handler func(s *state, cmd command, user database.User) 
 	return func(s *state, cmd command) error {
 		currentUser, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
 		if err != nil {
-			return err
+			return fmt.Errorf("error getting current user from database: %v", err)
 		}
 
 		return handler(s, cmd, currentUser)
