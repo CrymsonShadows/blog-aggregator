0e294f77ade4390d1f79744dd8d7d6f637446936
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2025-01-28 16:58:17 -0600

    add GetNextFeedToFetch to get the next feed to fetch

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -25,3 +25,7 @@ WHERE $1 = url;
 UPDATE feeds
 SET last_fetched_at = $2 AND updated_at = $2
 WHERE feeds.id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at DESC NULLS FIRST LIMIT 1;
