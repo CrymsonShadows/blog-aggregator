300ad87c20b690a815c23e3c01cbff9c29e11a2e
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2025-01-28 17:21:23 -0600

    add scrapeFeeds function that fetches and shows the items in the next feed

--- handler_agg.go
+++ handler_agg.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"time"
+
+	"github.com/CrymsonShadows/blog-aggregator/internal/database"
 )
 
 func handlerAgg(s *state, cmd command) error {
@@ -12,4 +16,33 @@ func handlerAgg(s *state, cmd command) error {
 	}
 	fmt.Println(feed)
 	return nil
+}
+
+func scrapeFeeds(s *state) {
+	nextFeed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		fmt.Println("was unable to get next feed to fetch from database")
+	}
+
+	currentTime := sql.NullTime{
+		Time:  time.Now(),
+		Valid: true,
+	}
+	err = s.db.MarkFeedFetched(context.Background(), database.MarkFeedFetchedParams{
+		ID:            nextFeed.ID,
+		LastFetchedAt: currentTime,
+	})
+	if err != nil {
+		fmt.Printf("error updating update time of feed in database: %v", err)
+	}
+
+	feed, err := fetchFeed(context.Background(), nextFeed.Url)
+	if err != nil {
+		fmt.Printf("error fetching from %v", err)
+	}
+
+	fmt.Printf("Feed: %s", feed.Channel.Title)
+	for i, item := range feed.Channel.Item {
+		fmt.Printf("Item %d: %s\n", i, item.Title)
+	}
 }
