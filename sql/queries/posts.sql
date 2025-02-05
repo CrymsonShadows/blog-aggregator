cf67a0cf53161d22b204a38f8608444041437b2d
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2025-02-05 17:03:35 -0600

    add query that gets posts from the feeds a user follows

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -11,3 +11,12 @@ VALUES (
     $8
 )
 RETURNING *;
+
+-- name: GetFeedByUserID :one
+WITH users_feed_follows AS (
+    SELECT * FROM feed_follows
+    WHERE $1 = feed_follows.user_id
+)
+SELECT * FROM posts
+WHERE users_feed_follows.feed_id = feed_id
+ORDER BY published_at DESC LIMIT $2;
